9ec6606b6ff3a573ad3f4d22e0e92769f4d6c9cb
lenovo <https://github.com/nuanfeng1/untitled1.git>
2020-08-05 12:01:16 +0800

    测试新

--- ceshi/src/ceshi/ceshi.java
+++ ceshi/src/ceshi/ceshi.java
@@ -4,6 +4,7 @@ public class ceshi {
     public static void main(String[] args){
         System.out.println("测试");
         System.out.println("在线更新");
+        System.out.println("123");
         
     }
 }
